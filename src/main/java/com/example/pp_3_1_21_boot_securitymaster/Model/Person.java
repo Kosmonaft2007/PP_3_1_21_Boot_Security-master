a56354be1529fed45b56e89894571d59be9f637e
Vadim <kosmonaft2007@ya.ru>
2023-06-16 04:25:37 +0300

    first commit

--- src/main/java/com/example/pp_3_1_21_boot_securitymaster/Model/Person.java
+++ src/main/java/com/example/pp_3_1_21_boot_securitymaster/Model/Person.java
@@ -6,7 +6,7 @@ import lombok.Data;
 //import javax.validation.constraints.*;
 
 
-//@Data
+@Data
 // обозначает что по умолчанию будут сгенирированы автоматически и ареализованы  GETor and SETTTor и конструкторы
 @Entity // обозначает что данный объект (сущность) связанна с БД
 @Table(name = "person")
@@ -34,46 +34,46 @@ public class Person {
 //    @Column(name = "age")
 //    private int age;
 
-    public Person() {
-
-    }
-
-    public Person(Long id, String firstName, String lastName) {
-        this.id = id;
-        this.firstName = firstName;
-        this.lastName = lastName;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    @Override
-    public String toString() {
-        return "Person{" +
-                "id=" + id +
-                ", firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                '}';
-    }
+//    public Person() {
+//
+//    }
+//
+//    public Person(Long id, String firstName, String lastName) {
+//        this.id = id;
+//        this.firstName = firstName;
+//        this.lastName = lastName;
+//    }
+//
+//    public Long getId() {
+//        return id;
+//    }
+//
+//    public void setId(Long id) {
+//        this.id = id;
+//    }
+//
+//    public String getFirstName() {
+//        return firstName;
+//    }
+//
+//    public void setFirstName(String firstName) {
+//        this.firstName = firstName;
+//    }
+//
+//    public String getLastName() {
+//        return lastName;
+//    }
+//
+//    public void setLastName(String lastName) {
+//        this.lastName = lastName;
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "Person{" +
+//                "id=" + id +
+//                ", firstName='" + firstName + '\'' +
+//                ", lastName='" + lastName + '\'' +
+//                '}';
+//    }
 }
